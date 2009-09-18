157d6e19b1f32d33f69aab7a981de88ffd81575b
Niclas Nilsson <niclas.nilsson@factor10.com>
2009-09-18 08:15:58 +0200

    Can load blocks and them in different in contexts

--- lib/dsl-block-engine.rb
+++ lib/dsl-block-engine.rb
@@ -3,4 +3,49 @@ $:.unshift(File.dirname(__FILE__)) unless
 
 module DslBlockEngine
   VERSION = '0.0.1'
+
+  class Context
+    def attr_accessor *names
+      names.each do |name|
+        self.class.send :attr_accessor, name
+      end
+    end
+  end
+  
+  class DslBlockEngine
+    def initialize
+      @blocks = {}
+    end
+    
+    def load code
+      self.instance_eval code
+    end
+
+    def blocks
+      @blocks
+    end
+    
+    def create_context *attrs
+      o = Object.new
+      attrs.each do |attr|
+        o.class.send :attr_accessor, attr
+      end
+      o 
+    end
+    
+    def execute_in_context ctx, category, event
+      b = @blocks[category][event]
+      ctx.instance_eval &b
+    end
+    
+    private
+
+    def on name, &block
+      puts "In on method: #{block.inspect}"
+      @blocks[:on] ||= {}
+      @blocks[:on][name] = block
+    end
+
+  end
+  
 end
--- spec/dsl-block-engine_spec.rb
+++ spec/dsl-block-engine_spec.rb
@@ -1,11 +1,39 @@
 require File.dirname(__FILE__) + '/spec_helper.rb'
 
-# Time to add your specs!
-# http://rspec.info/
-describe "Place your specs here" do
+describe DslBlockEngine do
   
-  it "find this spec in spec directory" do
-    # violated "Be sure to write your specs"
+  before do
+    @engine = DslBlockEngine::DslBlockEngine.new 
   end
   
+  it "should load named blocks" do
+    code = %q{
+      on :event1 do
+      end
+
+      on :event2 do
+      end
+    }
+    
+    @engine.load code
+    @engine.blocks[:on].size.should == 2
+  end
+
+  it "should provide properties accessible for the block" do
+    code = %q{
+      on :event1 do
+        a + b
+      end
+    }
+
+    @engine.load code
+    context = @engine.create_context :a, :b
+    
+    context.a = 2
+    context.b = 4
+    
+    result = @engine.execute_in_context context, :on, :event1
+    result.should == 6
+  end
 end
+
